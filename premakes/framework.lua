62109fe44271bb1680046c6685ee9832eb837da6
RichyHBM <git@richyhbm.co.uk>
2013-10-20 18:16:59 +0100

    Fix premakes

--- premakes/framework.lua
+++ premakes/framework.lua
@@ -12,6 +12,23 @@
 
       includedirs { "../external/include/"} 
 
-	  if _ACTION == "vs2010" then
-        postbuildcommands { "call ..\\postbuild\\vs.bat" }
+	   if _ACTION == "vs2010" then
+         postbuildcommands { "call ..\\postbuild\\vs.bat" }
       end    
+
+   configuration "Debug"
+      if _ACTION == "vs2010" then
+         links { "sfml-window-d", "sfml-system-d", "glu32", "opengl32", "noisepp-d" }
+      end 
+      if _ACTION == "gmake" then
+         links { "sfml-window", "sfml-system", "GLU", "GL", "noisepp-d" }
+      end
+
+
+   configuration "Release"
+      if _ACTION == "vs2010" then
+         links { "sfml-window", "sfml-system", "glu32", "opengl32", "noisepp" }
+      end
+      if _ACTION == "gmake" then
+         links { "sfml-window", "sfml-system", "GLU", "GL", "noisepp" }
+      end
